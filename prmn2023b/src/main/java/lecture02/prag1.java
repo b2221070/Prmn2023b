4c144dab14466e9b283e651428e09f12d903825e
b2221070 <b2221070@photon.chitose.ac.jp>
2023-11-01 12:04:53 +0900

    lecture2を作りました。‘ââ

---
+++ prmn2023b/src/main/java/lecture02/prag1.java
@@ -0,0 +1,16 @@
+package lecture02;
+
+import  java.util.Scanner;
+
+public class prag1 {
+    public static void main(String[] args) {
+        String number ="B2001000";
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("学籍番号を入力してください。:");
+        String gakuseki = scan.next();
+
+        Gakuseki gaku = new Gakuseki();
+        gaku.hantei(number,gakuseki);
+    }
+}
